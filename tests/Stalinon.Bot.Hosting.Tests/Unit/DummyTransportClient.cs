2dafc8b5f6d720d58c9403ebeb9e2bd8c9a662e3
Egor Fedorov <jtqafj@gmail.com>
2025-09-11 18:12:20 +0300

    chore(tests): add dummy transport client

---
+++ tests/Stalinon.Bot.Hosting.Tests/Unit/DummyTransportClient.cs
@@ -0,0 +1,61 @@
+using System.Diagnostics.CodeAnalysis;
+
+using Stalinon.Bot.Abstractions;
+using Stalinon.Bot.Abstractions.Addresses;
+using Stalinon.Bot.Abstractions.Contracts;
+
+namespace Stalinon.Bot.Hosting.Tests;
+
+/// <summary>
+///     Заглушка транспортного клиента для тестов.
+/// </summary>
+/// <remarks>
+///     <list type="number">
+///         <item>Используется для проверки взаимодействия с транспортом</item>
+///     </list>
+/// </remarks>
+[SuppressMessage("Performance", "CA1822:Пометьте члены как статические")]
+internal sealed class DummyTransportClient : ITransportClient
+{
+    /// <summary>
+    ///     Сообщения для тестов
+    /// </summary>
+    public List<(ChatAddress chat, string text)> SentTexts { get; } = new();
+
+    /// <inheritdoc />
+    public Task SendTextAsync(ChatAddress chat, string text, CancellationToken ct)
+    {
+        SentTexts.Add((chat, text));
+        return Task.CompletedTask;
+    }
+
+    /// <inheritdoc />
+    public Task SendPhotoAsync(ChatAddress chat, Stream photo, string? caption, CancellationToken ct)
+    {
+        return Task.CompletedTask;
+    }
+
+    /// <inheritdoc />
+    public Task EditMessageTextAsync(ChatAddress chat, long messageId, string text, CancellationToken ct)
+    {
+        return Task.CompletedTask;
+    }
+
+    /// <inheritdoc />
+    public Task EditMessageCaptionAsync(ChatAddress chat, long messageId, string? caption, CancellationToken ct)
+    {
+        return Task.CompletedTask;
+    }
+
+    /// <inheritdoc />
+    public Task SendChatActionAsync(ChatAddress chat, ChatAction action, CancellationToken ct)
+    {
+        return Task.CompletedTask;
+    }
+
+    /// <inheritdoc />
+    public Task DeleteMessageAsync(ChatAddress chat, long messageId, CancellationToken ct)
+    {
+        return Task.CompletedTask;
+    }
+}
